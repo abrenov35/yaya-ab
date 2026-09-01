f7b884d3f5511763235f2638a2bdc6b5adb3b9db
abrenov35 <abrenov35@gmail.com>
2026-09-01 09:25:24 +0200

    Supprime loader test DB OD hors périmètre

--- dd-db-search-loader.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  'use strict';
-  if (document.querySelector('script[data-yaya-dd-db-search]')) return;
-  var s=document.createElement('script');
-  s.src='dd-db-search-restore.js?v=20260901-1';
-  s.defer=true;
-  s.dataset.yayaDdDbSearch='1';
-  document.head.appendChild(s);
-})();
