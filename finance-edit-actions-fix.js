d76ee6a9b80177aa5b68ae18c1521469d73cb2c3
abrenov35 <abrenov35@gmail.com>
2026-09-16 17:17:26 +0200

    Load purchase creation fields fix

--- finance-edit-actions-fix.js
+++ finance-edit-actions-fix.js
@@ -12,6 +12,7 @@
 
   setTimeout(function(){
     [
+      ['achat-create-actions-fix.js?v=3','yaya-achat-create-actions-loader-v3'],
       ['yaya-central-authoritative-sync.js?v='+Date.now(),'yaya-central-authority-loader'],
       ['commande-modal-performance-fix.js?v=2','yaya-command-perf-loader'],
       ['mail-documents-force-render.js?v=4','yaya-mail-force-loader'],
