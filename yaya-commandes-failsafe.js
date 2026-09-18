e52dd5064510b80090202d24f67c12b7a63596d8
abrenov35 <abrenov35@gmail.com>
2026-09-19 01:29:26 +0200

    Remove legacy AB COMMANDES integration from Yaya

--- yaya-commandes-failsafe.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-  /* Ancienne intégration AB COMMANDES désactivée.
-     L'affichage est désormais géré exclusivement par yaya-ab-commandes-link.js. */
-  window.__yayaCommandesFailsafeV1=true;
-  window.__yayaCommandesFailsafeDisabled=true;
-})();
